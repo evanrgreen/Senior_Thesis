71b2bf5c16d76f8a2a6ba6ff78889fd69d412136
evanrgreen <evanrgreen@aol.com>
2018-01-01 21:30:30 -0500

    Fix Formatting and Sytle conventions

--- R Files/Install_Packages.R
+++ R Files/Install_Packages.R
@@ -1,7 +1,9 @@
-packages_to_install <- c("igraph", "poweRlaw","ggplot2",
-                         "randomForest","MASS",
-                         "dplyr","reshape2","visreg",
-                         "stargazer","RColorBrewer",
-                         "network","sna","ndtv",
-                         "visNetwork","stats4")
+packages_to_install <- c(
+  "igraph", "poweRlaw", "ggplot2",
+  "randomForest", "MASS",
+  "dplyr", "reshape2", "visreg",
+  "stargazer", "RColorBrewer",
+  "network", "sna", "ndtv",
+  "visNetwork", "stats4"
+)
 install.packages(packages_to_install)
